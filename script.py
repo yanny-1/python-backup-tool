c8dbc1147e7949ff0fbcb6a3766013b3cb90e82a
yanny-1 <marcinwojciechowski290@gmail.com>
2026-01-21 18:07:45 +0100

    script.py
    The script's language has been changed to English

--- script.py
+++ script.py
@@ -10,21 +10,23 @@ for task in plik:
     counter = 0
     source = Path(task["source"])
     backup = Path(task["backup"])
+    
     if not backup.exists(): 
         backup.mkdir(parents=True)
     else: 
-        print(f'{backup} już istnieje. ')
+        print(f'{backup} already exists.')
+    
     with open("log.txt", "a", encoding="utf-8") as f: 
         if not source.exists():
-            print(f'{source} nie istnieje! ')
-            x = input("Czy chcesz go utworzyć? tak/nie ")
+            print(f'{source} does not exist!')
+            x = input("Do you want to create it? yes/no ")
             match x: 
-                case "tak":
+                case "yes":
                     source.mkdir(parents=True)
-                case "nie": 
+                case "no": 
                     continue
                 case "": 
-                    print("Należy wpisać 'tak' lub 'nie'")
+                    print("You must enter 'yes' or 'no'")
         else: 
             for file in source.rglob("*"): 
                 file_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -32,11 +34,11 @@ for task in plik:
                     relative = file.relative_to(source)
                     target = backup / relative 
                     target.parent.mkdir(parents=True, exist_ok=True)
-                    if not target.exists() or  file.stat().st_mtime >  target.stat().st_mtime:
-                            shutil.copy(file, target)
-                            counter += 1 
-                            print(f"Pomyślnie skopiowano {file.name}")
+                    if not target.exists() or file.stat().st_mtime > target.stat().st_mtime:
+                        shutil.copy(file, target)
+                        counter += 1 
+                        print(f"Successfully copied {file.name}")
                     else: 
-                        print(f'{file.name} nie został skopiowany. ')
-                f.write(f'Data: {file_date} \n Skopiowano {file.name}\n')
-            f.write(f'[SUMMARY] - {counter} plików zostało skopiowanych. \n\n')
+                        print(f'{file.name} was not copied.')
+                f.write(f'Date: {file_date} \n Copied {file.name}\n')
+            f.write(f'[SUMMARY] - {counter} files were copied. \n\n')
